28e714eefb695361b95c997c849918bf1d73708e
Mathias Ringot <74365946+4n0m4lie@users.noreply.github.com>
2024-06-12 16:15:46 +0200

    Add files via upload

---
+++ docker/sql/1-webdirectory_schema.sql
@@ -0,0 +1,34 @@
+-- Adminer 4.2.2 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(128) NOT NULL,
+  `prenom` varchar(128) DEFAULT NULL,
+  `nbureau` int(4) DEFAULT NULL,
+  `tel_mobile` varchar(12) DEFAULT NULL,
+  `tel_fixe` varchar(12) DEFAULT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS departement;
+CREATE TABLE departement (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nom` varchar(128) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `utilisateur2departement`;
+CREATE TABLE utilisateur2departement (
+    `id_utilisateur` int(11) NOT NULL,
+    `id_departement` int(11) NOT NULL,
+    PRIMARY KEY (`id_departement`, `id_utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
